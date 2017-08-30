5174632f18650c79b71f724888dde792eb531d25
ent <xzzhangxx@sina.com>
2017-08-30 12:56:38 +0800

    second submit

--- etnweb1/src/com/ent/web/HelloWorld.java
+++ etnweb1/src/com/ent/web/HelloWorld.java
@@ -3,5 +3,6 @@ package com.ent.web;
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Ethan first project");
+		System.out.println("Ethan first project 2");
 	}
 }
