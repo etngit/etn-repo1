e6994819ed5de4e08daa71ead49cac9a5a577dfa
ent <xzzhangxx@sina.com>
2017-08-31 12:52:15 +0800

    Modify java file

--- etnweb1/src/com/ent/web/HelloWorld.java
+++ etnweb1/src/com/ent/web/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Ethan first project");
 		System.out.println("Ethan first project 2");
+		System.out.println("Ethan first project 3");
 	}
 }
